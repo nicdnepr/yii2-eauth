ab6716a1b9e934058a5c24e5ebf63817b910acce
Konstantin Tsukur <nicdnepr@gmail.com>
2016-12-17 09:40:00 +0200

    fix

--- src/views/redirect.php
+++ src/views/redirect.php
@@ -9,30 +9,37 @@ use yii\web\View;
 
 ?><!DOCTYPE html>
 <html>
-<head>
-    <script type="text/javascript">
-        <?php
-            $code = 'if (window.opener) {';
-            $code .= 'window.close();';
-            if ($redirect) {
-                $code .= 'window.opener.location = \''.addslashes($url).'\';';
-            }
-            $code .= '}';
-            $code .= 'else {';
-            if ($redirect) {
-                $code .= 'window.location = \''.addslashes($url).'\';';
-            }
-            $code .= '}';
-            $code .= "dataLayer.push({'event' : 'user-signup-success', 'method': '<" . Yii::$app->getRequest()->getQueryParam('service') . ">' });";
-            echo $code;
-        ?>
-    </script>
-</head>
+<head></head>
 <body>
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WK6P54" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+<!-- Google Tag Manager -->
+<script>
+
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5WK6P54');
+
+
+    <?php
+    $code = 'if (window.opener) {';
+    $code .= 'window.close();';
+    if ($redirect) {
+        $code .= 'window.opener.location = \''.addslashes($url).'\';';
+    }
+    $code .= '}';
+    $code .= 'else {';
+    if ($redirect) {
+        $code .= 'window.location = \''.addslashes($url).'\';';
+    }
+    $code .= '}';
+    $code .= "dataLayer.push({'event' : 'user-signup-success', 'method': '" . Yii::$app->getRequest()->getQueryParam('service') . "' });";
+    echo $code;
+    ?>
+    
+</script>
+<!-- End Google Tag Manager -->
 
 <h2 id="title" style="display:none;">
     <?php echo \Yii::t('eauth', 'Redirecting back to the application...'); ?>
